1dd95c3010c92f905ab3b002d06fd0a3517728d8
Ram Jagana <ram.jagana@gmail.com>
2024-03-09 21:14:36 -0500

    commit rest of changes

---
+++ src/test/java/com/devops/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.devops;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		assertTrue("implementation goes here", true);
+	}
+
+}
